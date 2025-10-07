ade846fc7b437a587f9e4ff5a105a506e6a85c23
Iqbal Firmansyah Adinda <134963689+IqbalFirmasnyah@users.noreply.github.com>
2025-10-07 22:53:49 +0700

    push

--- src/app/admin/paket-wisata/page.tsx
+++ src/app/admin/paket-wisata/page.tsx
@@ -137,7 +137,8 @@ export default function AdminPaketWisataPage() {
       if (!token) throw new Error('Token tidak ditemukan.');
 
       let res: Response;
-      const url = editingPaket ? `${process.env.NEXT_PUBLIC_API_URL}/paket-wisata/${editingPaket.paketId}` : `${process.env.NEXT_PUBLIC_API_URL}/paket-wisata/add`;
+      const api =process.env.NEXT_PUBLIC_API_URL
+      const url = editingPaket ? `${api}/paket-wisata/${editingPaket.paketId}` : `${api}/paket-wisata/add`;
       const method = editingPaket ? 'PATCH' : 'POST';
 
       res = await fetch(url, {
